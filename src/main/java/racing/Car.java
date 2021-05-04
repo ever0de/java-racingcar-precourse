b523bdbeba20289940702390a565208d450e2e8e
ever0de <jiseok.dev@gmail.com>
2021-05-04 12:37:49 +0900

    feat: Car
    - 자동차 주행거리 추가

---
+++ src/main/java/racing/Car.java
@@ -0,0 +1,42 @@
+package racing;
+
+import java.util.Random;
+
+public class Car {
+
+    private static final int FORWARD_MINIMUM_NUMBER = 4;
+    private static final int MAXIMUM_NUMBER = 10;
+    private final CarName carName;
+    private final CarMileage mileage;
+
+    private static final Random rand = new Random();
+
+    public Car(String name) {
+        carName = new CarName(name);
+        mileage = new CarMileage();
+    }
+
+    public void forward() {
+        int number = generateRandomNumber();
+        if (isForward(number)) {
+            mileage.add();
+        }
+    }
+
+    public boolean isForward(int number) {
+        return number >= FORWARD_MINIMUM_NUMBER;
+    }
+
+    public String getCarName() {
+        return carName.getCarName();
+    }
+
+    public int getMileageNumber() {
+        return mileage.getCarMileage();
+    }
+
+    private int generateRandomNumber() {
+        return rand.nextInt(MAXIMUM_NUMBER);
+    }
+
+}
---
+++ src/main/java/racing/CarMileage.java
@@ -0,0 +1,14 @@
+package racing;
+
+public class CarMileage {
+
+    private int mileage = 0;
+
+    public int getCarMileage() {
+        return mileage;
+    }
+
+    public void add() {
+        mileage++;
+    }
+}
